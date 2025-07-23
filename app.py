be144fc97d1142f0279aa1064255a1c556c4d91d
NAVADEEP5784 <150226119+NAVADEEP5784@users.noreply.github.com>
2025-07-23 14:51:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -242,4 +242,4 @@ def delete_bill(bill_id):
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 13000)))
